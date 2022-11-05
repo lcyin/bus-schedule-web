0207061a9ecfc4a0b46792ce40875cbc233a9a9a
lcyin <laichunyinkevin@gmailcom>
2022-11-05 14:58:26 +0800

    fix new-link

--- components/RouteItem.js
+++ components/RouteItem.js
@@ -5,11 +5,11 @@ import routeStyles from "../styles/Route.module.css";
 const RouteItem = ({ route }) => {
   return (
     <li>
-      <Link as={`/route/${route.id}`} href="/route/[id]">
-        <a className={routeStyles.card}>
-          <h3>{route.route}</h3>
-          <p>To: {route.dest_tc}</p>
-        </a>
+      <Link as={`/route/${route.id}`} href="/route/[id]" className={routeStyles.card}>
+
+        <h3>{route.route}</h3>
+        <p>To: {route.dest_tc}</p>
+
       </Link>
     </li>
   );
